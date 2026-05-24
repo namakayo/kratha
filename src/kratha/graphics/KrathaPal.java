3fb0978d3b3aa01285b5edf4d38e82f02e2ccad5
yroo <purnamaaleron7@gmail.com>
2026-05-24 18:15:01 +0700

    Update KrathaPal.java

--- src/kratha/graphics/KrathaPal.java
+++ src/kratha/graphics/KrathaPal.java
@@ -4,6 +4,7 @@ import arc.graphics.Color;
 
 public class KrathaPal{
     public static Color
+    krathaOutline = color.valueOf("4E4C52"),
     krathiteDark = Color.valueOf("51436B"),
     krathite = Color.valueOf("897090"),
     krathiteLight = Color.valueOf("B3A0B6");
