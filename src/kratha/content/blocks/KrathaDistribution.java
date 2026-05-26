b3abffd791ff5740cacb22ae9858ab42832f5e28
yroo <purnamaaleron7@gmail.com>
2026-05-26 14:59:34 +0700

    Create KrathaDistribution.java

---
+++ src/kratha/content/blocks/KrathaDistribution.java
@@ -0,0 +1,38 @@
+package kratha.content.deterra.blocks;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.distribution.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import mindustry.content.*;
+import kratha.world.blocks.distribution.*;
+import kratha.content.KrathaItems;
+
+import static mindustry.type.ItemStack.with;
+
+public class DeterraDistribution {
+    public static Block
+            itemTube, itemOverpass;
+    public static void load() {
+        {
+            {
+                itemTube = new ItemTube("item-tube"){{
+                    requirements(Category.distribution, with(KrathaItems.krathite, 1));
+                    speed = 0.065f;
+                    displayedSpeed = 9f;
+                    bridgeReplacement=itemOverpass;
+                    researchCost = with(KrathaItems.krathite, 5);
+                }};
+                itemOverpass = new ItemOverpass("item-overpass"){{
+                    requirements(Category.distribution, with(KrathaItems.krathite, 6, KrathaItems.guartz, 3));
+                    range=4;
+                    researchCost = with(KrathaItems.krathite, 50,KrathaItems.guartz,40);
+                }};
+            }
+        }
+    }
+                                        }
