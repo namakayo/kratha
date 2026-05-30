bba249f3a813aa52439136b24ca0aa0701b8ffa3
yroo <purnamaaleron7@gmail.com>
2026-05-30 16:17:58 +0700

    wakaruwakaruonajikimochisa

---
+++ src/kratha/content/blocks/KrathaProduction.java
@@ -0,0 +1,35 @@
+package kratha.content.blocks;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.production.*;
+import mindustry.world.meta.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import mindustry.content.*;
+import mindustry.world.draw.*;
+import mindustry.type.*;
+import kratha.content.KrathaItems;
+import kratha.graphics.KrathaPal;
+
+import static mindustry.type.ItemStack.with;
+
+public class KrathaProduction {
+    public static Block
+    drill;
+    public static void load() {
+        drill = new Drill("drill"){{
+            requirements(Category.production, with(KrathaItems.krathite, 10, KrathaItems.guartz, 5));
+            consumePower(10/60f);
+            
+            drillTime = 380;
+            tier = 2;
+            size = 2;
+            fogRadius = 0;
+            researchCost = with(KrathaItems.krathite, 10, KrathaItems.guartz, 5);
+        }};
+    }
+}
