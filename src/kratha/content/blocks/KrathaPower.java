b4e2d84d4bf755c348159e2b4362d498c09f2539
yroo <purnamaaleron7@gmail.com>
2026-05-31 11:39:26 +0700

    Create KrathaPower.java

---
+++ src/kratha/content/blocks/KrathaPower.java
@@ -0,0 +1,38 @@
+package kratha.content..blocks;
+
+import mindustry.type.Category;
+import mindustry.world.Block;
+import mindustry.world.blocks.power.*;
+import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+import mindustry.content.*;
+import mindustry.gen.*;
+import kratha.content.KrathaItems;
+import kratha.world.blocks.power.*;
+
+import static mindustry.type.ItemStack.with;
+
+public class KrathaPower{
+    public static Block
+    relay, windTurbine;
+
+    public static void load(){
+        relay = new PowerNode("relay"){{
+            requirements(Category.power, with(KrathaItems.guartz, 5));
+            health = 20;
+            size = 1;
+            squareSprite = false;
+            laserRange = 10f;
+            maxNodes = 8;
+            researchCost = with(KrathaItems.guartz,5);
+        }};
+        windTurbine = new WindTurbine("wind-turbine"){{
+            requirements(Category.power, with(KrathaItems.krathite, 25, KrathaItems.guartz, 40));
+            size = 2;
+            squareSprite = false;
+            powerProduction = 40f/60f;
+            drawer = new DrawMulti(new DrawDefault(), new DrawRegion("-rotator", 4, true));
+            researchCost = with(KrathaItems.krathite,25,KrathaItems.guartz,40);
+        }};
+    }
+}
