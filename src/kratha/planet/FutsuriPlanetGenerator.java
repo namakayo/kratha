fb365a15daa43070be4dd7533b30579cf873f99e
yroo <purnamaaleron7@gmail.com>
2026-06-11 17:28:40 +0700

    Create FutsuriPlanetGenerator.java

---
+++ src/kratha/planet/FutsuriPlanetGenerator.java
@@ -0,0 +1,32 @@
+package kratha.planet;
+
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.util.Tmp;
+import arc.util.noise.*;
+import kratha.content.deterra.blocks.KrathaEnv;
+import mindustry.content.Blocks;
+import mindustry.maps.generators.PlanetGenerator;
+import mindustry.world.Block;
+
+public class FutsuriPlanetGenerator extends PlanetGenerator {
+    public float heightScl = 0.7f, octaves = 6, persistence = 0.6f, heightPow = 2f, heightMult = 1.1f;
+    
+    Vec3 craterPos = new Vec3(0.1503837331804,0.852868531952,0.5);
+    @Override
+    public float getHeight(Vec3 position) {
+        return Mathf.pow(rawHeight(position), heightPow) * heightMult;
+    }
+
+    float rawHeight(Vec3 position) {
+        return Simplex.noise3d(seed, octaves, persistence, 1f / heightScl, 10f + position.x, 10f + position.y, 10f + position.z) - (position.dst(craterPos)<0.4f?0.4f-((float)-Math.pow(position.dst(craterPos),8)+(float)Math.pow(position.dst(craterPos),4)*Mathf.sqrt(2)):0f);
+    }
+
+    @Override
+    public void getColor(Vec3 position, Color out) {
+        Block block = rawHeight(position) < 0.4f ? KrathaEnv.krathagenDeep : rawHeight(position) < 0.5f ? KrathaEnv.krathagenFloor : rawHeight(position) < 0.6f ? KrathaEnv.krathiteFloor : rawHeight(position) < 0.7f ? KrathaEnv.krathiteRough : KrathaEnv.eonstoneErodedFloor;
+
+        out.set(block.mapColor).a(1f - block.albedo);
+    }
+}
