7facc8174c8caa5c9dfce301ff0c06dd0df68bc5
yroo <purnamaaleron7@gmail.com>
2026-06-15 18:28:22 +0700

    Update KrathaSectorPresets.java

--- src/kratha/content/KrathaSectorPresets.java
+++ src/kratha/content/KrathaSectorPresets.java
@@ -5,7 +5,7 @@ import kratha.type.KrathaSectorPreset;
 import mindustry.type.SectorPreset;
 
 public class KrathaSectorPresets {
-    public static KrathaSectorPreset theFountain,oasis;
+    public static KrathaSectorPreset theFountain,oasis,harbor;
 
     public static void load(){
         theFountain = new KrathaSectorPreset("the-fountain", KrathaPlanets.kratha, 452){{
@@ -15,6 +15,10 @@ public class KrathaSectorPresets {
         }};
         oasis = new KrathaSectorPreset("oasis", KrathaPlanets.kratha, 32){{
            difficulty = 1;
+           overrideLaunchDefaults = true;
+        }};
+        harbor = new KrathaSectorPreset("harbor", KrathaPlanets.kratha, 456){{
+           difficulty = 2;
            overrideLaunchDefaults = true;
         }};
     }
