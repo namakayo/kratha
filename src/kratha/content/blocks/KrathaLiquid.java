14c83f3e98eda96426adb1bd4933200ce7377b93
yroo <purnamaaleron7@gmail.com>
2026-06-17 17:11:09 +0700

    Create KrathaLiquid.java

---
+++ src/kratha/content/blocks/KrathaLiquid.java
@@ -0,0 +1,30 @@
+package kratha.content.blocks;
+
+import arc.graphics.Color;
+import mindustry.world.Block;
+import mindustry.world.blocks.liquid.*;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.content.*;
+import mindustry.graphics.*;
+import mindustry.gen.Sounds;
+import mindustry.content.*;
+import kratha.world.blocks.liquid.*;
+import kratha.content.KrathaItems;
+
+import static mindustry.type.ItemStack.with;
+
+public class KrathaLiquid {
+    public static Block
+            liquidTube;
+    public static void load() {
+        {
+            {
+                liquidTube = new LiquidTube("liquid-tube"){{
+                    requirements(Category.liquid, with(KrathaItems.krathite, 2));
+                    researchCost = with(KrathaItems.krathite, 100);
+                }};
+            }
+        }
+    }
+                      }
