369d66d6b84de9ed32f7d3ddca0f8956455e2f72
yroo <purnamaaleron7@gmail.com>
2026-06-20 14:56:27 +0700

    Create DualTurbine.java

---
+++ src/kratha/world/blocks/power/DualTurbine.java
@@ -0,0 +1,38 @@
+package kratha.world.blocks.power;
+
+import arc.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.util.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import mindustry.world.blocks.power.ConsumeGenerator
+
+//very weird naming...
+public class DualTurbine extends ConsumeGenerator{
+    public Attribute attribute = Attribute.steam;
+
+    public DualTurbine(String name){
+        super(name);
+    }
+
+    @Override
+    public void drawPlace(int x, int y, int rotation, boolean valid){
+        super.drawPlace(x, y, rotation, valid);
+    }
+  
+    public class DualTurbineBuild extends ConsumeGeneratorBuild{
+
+        @Override
+        public void updateTile(){
+            super.updateTile();
+          
+            if(sumAttribute(attribute, tile.x, tile.y)>0.99f)liquids.add(ventLiquid.liquid, liquidCapacity - liquids.get(outputLiquid.liquid));
+        }
+    }
+}
