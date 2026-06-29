0ab26187b4a427470b4b1099a9ac3fd025c542cb
yroo <purnamaaleron7@gmail.com>
2026-06-29 17:18:17 +0700

    Update OreCluster.java

--- src/kratha/world/blocks/environment/OreCluster.java
+++ src/kratha/world/blocks/environment/OreCluster.java
@@ -17,6 +17,11 @@ import mindustry.world.blocks.environment.*;
 import static mindustry.Vars.*;
 
 public class OreCluster extends Block{
+    public float shadowOffset = -3f;
+    public float layer = Layer.power + 1;
+    public float shadowLayer = Layer.power - 1;
+    public float rotationRand = 20f;
+    public float shadowAlpha = 0.6f;
     public static final Point2[] offsets = {
         new Point2(0, 0),
         new Point2(1, 0),
@@ -38,13 +43,36 @@ public class OreCluster extends Block{
     public OreCluster(String name){
         super(name);
         variants = 2;
+        solid = true;
+        clipSize = 90;
+        customShadow = true;
     }
 
     @Override
-    public void drawMain(Tile tile){
-        if(checkAdjacent(tile)){
-            Draw.rect(variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))], tile.worldx() - tilesize, tile.worldy() - tilesize);
-        }
+    public void drawBase(Tile tile){
+        if(isCenter(tile));
+        float rot = Mathf.randomSeedRange(tile.pos() + 1, rotationRand);
+
+        Draw.z(shadowLayer);
+        Draw.color(0f, 0f, 0f, shadowAlpha);
+        Draw.rect(variants > 0 ? variantShadowRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantShadowRegions.length - 1))] : customShadowRegion,
+            tile.worldx() + shadowOffset, tile.worldy() + shadowOffset, rot);
+
+        Draw.color();
+
+        Draw.z(layer);
+        Draw.rect(variants > 0 ? variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))] : region,
+            tile.worldx(), tile.worldy(), rot);
+    }
+
+    @Override
+    public void drawShadow(Tile tile){
+
+    }
+
+    @Override
+    public TextureRegion[] icons(){
+        return variants == 0 ? super.icons() : new TextureRegion[]{Core.atlas.find(name + "1")};
     }
 
     public boolean isCenter(Tile tile){
