9aeff5f8b04e4f0077b97d07b24dfd9ce61a9e02
yroo <purnamaaleron7@gmail.com>
2026-07-05 08:35:23 +0700

    Create AncientLiquidTube.java

---
+++ src/kratha/world/blocks/liquid/AncientLiquidTube.java
@@ -0,0 +1,36 @@
+package kratha.world.blocks.liquid;
+
+import mindustry.gen.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.state;
+
+public class AncientLiquidTube extends LiquidTube{
+    public AncientCrafter(String name){
+        super(name);
+    }
+
+    @Override
+    public boolean canBreak(Tile tile){
+        return state.rules.editor;
+    }
+
+    public class AncientLiquidTubeBuild extends LiquidTubeBuild{
+        
+        @Override
+        public void damage(float damage){
+            return; //no damage
+        }
+
+        @Override
+        public boolean canPickup(){
+            return false; //no
+        }
+
+        @Override
+        public boolean collide(Bullet other){
+            return false; //no
+        }
+    }
+              }
