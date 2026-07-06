9317cc2d57a256b58b21d946a433a5777ee404d9
yroo <purnamaaleron7@gmail.com>
2026-07-06 14:52:52 +0700

    Update KrathaSectorPresets.java

--- src/kratha/content/KrathaSectorPresets.java
+++ src/kratha/content/KrathaSectorPresets.java
@@ -5,7 +5,7 @@ import kratha.type.KrathaSectorPreset;
 import mindustry.type.SectorPreset;
 
 public class KrathaSectorPresets {
-    public static KrathaSectorPreset theFountain,oasis,harbor,offshore;
+    public static KrathaSectorPreset theFountain,comingSoon;
 
     public static void load(){
         theFountain = new KrathaSectorPreset("the-fountain", KrathaPlanets.kratha, 452){{
@@ -13,17 +13,8 @@ public class KrathaSectorPresets {
            difficulty = 1;
            overrideLaunchDefaults = true;
         }};
-        oasis = new KrathaSectorPreset("oasis", KrathaPlanets.kratha, 32){{
-           difficulty = 1;
-           overrideLaunchDefaults = true;
-        }};
-        harbor = new KrathaSectorPreset("harbor", KrathaPlanets.kratha, 456){{
-           difficulty = 2;
-           overrideLaunchDefaults = true;
-        }};
-        offshore = new KrathaSectorPreset("offshore", KrathaPlanets.kratha, 360){{
-           difficulty = 3;
-           captureWave = 10;
+        comingSoon = new KrathaSectorPreset("coming-soon", KrathaPlanets.kratha, 32){{
+           difficulty = 0;
            overrideLaunchDefaults = true;
         }};
     }
