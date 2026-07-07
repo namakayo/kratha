669a333b3def5311774aae124b9a95678be55392
yroo <purnamaaleron7@gmail.com>
2026-07-07 08:24:03 +0700

    Update AncientMessageBlock.java

--- src/kratha/world/blocks/logic/AncientMessageBlock.java
+++ src/kratha/world/blocks/logic/AncientMessageBlock.java
@@ -24,6 +24,11 @@ public class AncientMessageBlock extends MessageBlock{
     }
 
     @Override
+    public boolean synthetic(){
+        return false;
+    }
+
+    @Override
     public boolean accessible(){
         return state.rules.editor;
     }
