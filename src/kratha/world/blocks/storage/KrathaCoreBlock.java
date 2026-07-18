297ecdcc27950a2b5dfea1b549169c20cd4ccb87
namaka <purnamaaleron7@gmail.com>
2026-07-18 15:10:13 +0700

    Create KrathaCoreBlock.java

---
+++ src/kratha/world/blocks/storage/KrathaCoreBlock.java
@@ -0,0 +1,17 @@
+package kratha.world.blocks.storage;
+
+import mindustry.world.blocks.storage.CoreBlock;
+import static mindustry.Vars.*;
+
+public class KrathaCoreBlock extends CoreBlock {
+    public KrathaCoreBlock(String name){
+        super(name);
+    }
+    
+    public class KrathaCoreBuild extends CoreBuild {
+      @Override
+      public void updateTile(){
+        state.rules.worldProcessorPlayerLink = true; //im genius
+      }
+    }
+ }     
