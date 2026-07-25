c1c24cf8ba56fdddcb5f382c3cc4355f60d84566
namakayo <purnamaaleron7@gmail.com>
2026-07-25 20:04:11 +0700

    update KrathaUnits.java

--- src/kratha/content/blocks/KrathaUnits.java
+++ src/kratha/content/blocks/KrathaUnits.java
@@ -44,7 +44,7 @@ public class KrathaUnits {
                 }};
                 rocketInstaller = new AncientReconstructor("rocket-installer"){{
                     requirements(Category.units, BuildVisibility.sandboxOnly, with());
-                    size = 4;
+                    size = 6;
                     consumeItems(with(KrathaItems.arkscrap, 12, KrathaItems.cobalt, 9));
                     consumeLiquid(KrathaLiquids.terac, 9f / 60f);
                     constructTime = 60f * 10f;
