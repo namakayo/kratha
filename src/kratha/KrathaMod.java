77a4b8f63fb86c95b720750a15270b3e16801193
namakayo <purnamaaleron7@gmail.com>
2026-08-04 19:58:41 +0700

    update KrathaMod.java

--- src/kratha/KrathaMod.java
+++ src/kratha/KrathaMod.java
@@ -16,21 +16,12 @@ public class KrathaMod extends Mod{
 
     public KrathaMod(){
         Log.info("They are watching."); //no reason for this lol
-        loadSettings();
-        /*
-        Events.on(ClientLoadEvent.class, e -> {
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("Notice");
-                dialog.cont.add("This is a very early version of KRATHA. Play at your own risk.").row();
-                dialog.cont.button("alr bro", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
+        Events.on(ClientLoadEvent.class,e->{
+            loadSettings();
         });
-        */
-        
     }
-    private void loadSettings(){
-        Vars.ui.settings.addCategory(Core.bundle.get("settings.kratha-title"), Icon.book, t -> {
+    public void loadSettings(){
+        ui.settings.addCategory(Core.bundle.get("settings.kratha-title"), Icon.book, t -> {
             t.checkPref("@settings.terraplasm-item-debug", false);
         });
     }
