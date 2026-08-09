10896aab8a538256a17dac6a8f6f1c2add826d43
namakayo <purnamaaleron7@gmail.com>
2026-08-09 07:52:30 +0700

    update BioHeart.java

--- src/kratha/world/blocks/terraplasm/BioHeart.java
+++ src/kratha/world/blocks/terraplasm/BioHeart.java
@@ -90,7 +90,7 @@ public class BioHeart extends BioBlock {
                     }
                 }
                 int itemss=content.items().size;
-                int totalHearts=getHearts();
+                int totalHearts=getHearts().size;
                 if(totalHearts!=lastTotalHearts){
                     lastTotalHearts=totalHearts;
                     hearts=getHeartsReachable();
