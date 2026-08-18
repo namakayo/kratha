7be41118c273a9f891ef068f9ce0038751f703f5
namakayo <purnamaaleron7@gmail.com>
2026-08-18 17:58:00 +0700

    update PanelLogger.java

--- src/kratha/world/blocks/logic/PanelLogger.java
+++ src/kratha/world/blocks/logic/PanelLogger.java
@@ -152,7 +152,6 @@ public class PanelLogger extends Block{
                 reqChipAmount+=(reqChip-p.items.get(chip));
                 reqChipTotal+=reqChip;
             }
-            Log.info(reqChipAmount);
             if(p.progress>=p.hackTime&&reqChipAmount<=0){
                 p.progress=p.hackTime;
                 progress=1;
